d3facf6e2f83ffccadc3003fed682a1a73b91dac
Thiago Felipe da Cruz <thiagofelipedacruz20@gmail.com>
2025-06-13 13:52:17 -0300

    Delete DesafioEX7-2.py

--- DesafioEX7-2.py
+++
@@ -1,8 +0,0 @@
-dado = 10
-print(f"Conteúdo da Variável dado: {dado}")
-
-dado += 1 # dado = dado + 1
-print(f"Conteúdo da Variável dado, depois do incremento: {dado}")
-
-dado -= 1 # dado = dado + 1
-print(f"Conteudo da Variável dado, depois do decremento: {dado}")
