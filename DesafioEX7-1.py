bf1f43dbf705daf623a6d0211aad5dbe10675d53
Thiago Felipe da Cruz <thiagofelipedacruz20@gmail.com>
2025-06-13 13:47:55 -0300

    Add files via upload

---
+++ DesafioEX7-1.py
@@ -0,0 +1,15 @@
+A = float(input("Digite o primeiro número: "))
+B = float(input("Digite o segundo número: "))
+
+soma = A + B
+subtracao = A - B
+multiplicacao = A * B
+divisao = A / B
+resto = A % B
+
+print("Resultados: ")
+print("Soma: ", soma)
+print("Subtração: ", subtracao)
+print("Multiplicação: ", multiplicacao)
+print("Divisão: ", divisao)
+print("Resto da Divisão: ", resto)
---
+++ DesafioEX7-3.py
@@ -0,0 +1,8 @@
+dado = 10
+print(f"Conteúdo da Variável dado: {dado}")
+
+dado *= 2 # dado = dado * 1
+print(f"Conteúdo da Variável dado, depois do incremento: {dado}")
+
+dado /= 2 # dado = dado / 1
+print(f"Conteudo da Variável dado, depois do decremento: {dado}")
